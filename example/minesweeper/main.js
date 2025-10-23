a06eec5f7249ebc10482b427114224d41846ff22
Canadadry <moureyjerome-spam@yahoo.fr>
2025-10-23 13:05:45 +0200

    zoom and center minesweeper game

--- example/minesweeper/main.js
+++ example/minesweeper/main.js
@@ -26,7 +26,26 @@ function init(){
   restart();
 }
 
+function computeView(w, h, lw, lh, tw, th) {
+  var mapWidth = lw * tw;
+  var mapHeight = lh * th;
+  var zoom = Math.min(w / mapWidth, h / mapHeight);
+  var offset_x = (w - mapWidth * zoom) / 2;
+  var offset_y = (h - mapHeight * zoom) / 2;
+
+  return { offset_x: offset_x, offset_y: offset_y, zoom:zoom };
+}
+
+function getTileAt(mouse_x, mouse_y, offset_x, offset_y, zoom, tw, th) {
+  var xInMap = (mouse_x - offset_x) / zoom;
+  var yInMap = (mouse_y - offset_y) / zoom;
+  var i = Math.floor(xInMap / tw);
+  var j = Math.floor(yInMap / th);
+  return { i:i, j:j };
+}
+
 function render() {
+  var view = computeView(window.width, window.height, size, size, tilesize, tilesize);
   ClearBackground({ r: 128, g: 128, b: 128 });
   for (var i = 0; i < size;i++){
     for (var j = 0; j < size;j++){
@@ -36,7 +55,10 @@ function render() {
       DrawImagePro(
         "minesweeper.png",
         { x: tile_x*tilesize, y: tile_y*tilesize, w: tilesize, h: tilesize },
-        { x: i*tilesize, y: j*tilesize, w: tilesize, h: tilesize }
+        {
+          x: i*tilesize*view.zoom+view.offset_x,
+          y: j*tilesize*view.zoom+view.offset_y,
+          w: tilesize*view.zoom, h: tilesize*view.zoom }
       );
     }
   }
@@ -48,19 +70,18 @@ function render() {
 }
 
 function click(x, y, button) {
-  var x_pos = Math.floor(x / tilesize);
-  var y_pos = Math.floor(y / tilesize);
-
   if (!game.inState(game,"playing")) {
     restart();
     return;
   }
-  var ret = false
+  var view = computeView(window.width, window.height, size, size, tilesize, tilesize);
+  var pos = getTileAt(x, y, view.offset_x, view.offset_y, view.zoom, tilesize, tilesize);
+  var ret = false;
 
   if (button == "left") {
-    ret = game.reveal(game,x_pos, y_pos)
+    ret = game.reveal(game, pos.i, pos.j);
   } else {
-    ret = game.flag(game,x_pos, y_pos)
+    ret = game.flag(game, pos.i, pos.j);
   }
 
   if (game.inState(game,"win")) {
